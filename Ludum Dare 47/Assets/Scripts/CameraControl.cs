206150facac194c3419cd65343f32bced313aa29
Angharad Hill <h017026h@student.staffs.ac.uk>
2020-10-03 14:40:29 +0100

    Player Movement and Camera Control
    movement is fixed controls, not based on camera rotation

---
+++ Ludum Dare 47/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float forwardForce = 10f;
+    public float sidewaysForce = 10f;
+
+    public Transform player;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Rotate around the player
+       
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            // transform.rotation = butts;
+            //current roatation + some around the player
+            //rotate the offset vector
+            transform.RotateAround(player.position, Vector3.up, -20 * Time.deltaTime);
+            offset = transform.position - player.position;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.RotateAround(player.position, Vector3.up, 20 * Time.deltaTime);
+            offset = transform.position - player.position;
+
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+           
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            
+        }
+
+    }
+
+    private void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
---
+++ Ludum Dare 47/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float forwardForce = 20f;
+    public float sidewaysForce = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey("d"))
+        {
+            GetComponent<Rigidbody>().AddForce(forwardForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            GetComponent<Rigidbody>().AddForce(-forwardForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            GetComponent<Rigidbody>().AddForce(0, 0, sidewaysForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            GetComponent<Rigidbody>().AddForce(0, 0, -sidewaysForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+    }
+}
